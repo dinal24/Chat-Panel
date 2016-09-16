e03787a95ca28785b9670a0665c6495b054d3d04
dinal24 <kdinal24@gmail.com>
2016-09-16 13:32:25 +0530

    v0.1

---
+++ sample/app.js
@@ -0,0 +1,63 @@
+(function() {
+  'use strict';
+
+  angular.module('app', ['botfac.chatPanel']);
+  angular.module('app').controller('Shell', Shell);
+
+  function Shell() {
+
+    var vm = this;
+
+    vm.messages = [
+      {
+        'username': 'Bot',
+        'content': 'Hi! I am here to help you'
+      },
+    ];
+
+    vm.username = 'You';
+
+    vm.syncPost = function(message){
+      var JSONObj = new Object();
+      JSONObj.question = message;
+      var url = "http://localhost:5000/answer";
+      var method = "POST";
+      var postData  = JSON.stringify(JSONObj);
+      var reply = "Error";
+      var async = false;
+      var request = new XMLHttpRequest();
+
+      console.log(JSONObj);
+      request.onload = function () {
+        var status = request.status;
+        var data = request.responseText;
+        console.log(status);
+        reply = JSON.parse(data).BotResponse;
+      };
+      request.open(method, url, async);
+      request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+      request.send(postData);
+      return reply;
+    };
+
+    vm.echoReversed = function (message) {
+      return message.split('').reverse().join('');
+    };
+
+    vm.sendMessage = function(message, username) {
+      if(message && message !== '' && username) {
+        vm.messages.push({
+          'username': username,
+          'content': message
+        });
+         vm.messages.push({
+           'username': "Bot",
+           'content': vm.echoReversed(message)
+        });
+
+      }
+    };
+
+  }
+
+})();
